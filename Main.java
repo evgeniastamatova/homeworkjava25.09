0b62811622b414307184f002fb8cf923b5faf101
evgeniastamatova <113890948+evgeniastamatova@users.noreply.github.com>
2022-09-25 15:43:38 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //First level: В методе main инициализировать все примитивные типы и вывести
+        // их в консоль, так чтоб появился такой результат(после ":"):
+        //
+        //char: Ginteger: 89
+        //byte: 4
+        //short: 56
+        //float: 4.7333436
+        //double: 4.355453532
+        //long: 12121
+        //
+        //
+        //Second level: Дано трехзначное число. Вывести на экран все цифры этого числа (сотни, десятки, единицы),
+        // используя математические операции. Например, для числа 365 необходимо с помощью математических операций
+        // получить такие значения, что сотен будет 3, десятков будет 6, единиц будет 5
+
+        char Ginteger = 89;
+        System.out.println(Ginteger);
+        String ginteger = "89";
+        System.out.println(ginteger);
+        byte a = 4;
+        System.out.println(a);
+        short b = 56;
+        System.out.println(b);
+        float c =  4.7333436f;
+        System.out.println(c);
+        double d = 4.355453532;
+        System.out.println(d);
+        long f = 12121;
+        System.out.println(f);
+
+};
+
+    }
